a1be087451a7109365dfe809bd57d7d85464b39a
LabhDeveloper <labh.k2003@gmail.com>
2022-09-27 21:38:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.custo
     params['CUST_ID'] = paymentDetails.customerId;
     params['TXN_AMOUNT'] = paymentDetails.amount;
     /* where is app is hosted (heroku url)*/
-    params['CALLBACK_URL'] = 'http://localhost:4100/callback';
+    params['CALLBACK_URL'] = 'https://zom-jwt.herokuap.com/callback';
     params['EMAIL'] = paymentDetails.customerEmail;
     params['MOBILE_NO'] = paymentDetails.customerPhone;
   
